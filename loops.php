021373beb0207e0fc720db2b213697fbdce12aa5
Ismael Perez <ismaelp@vsisoft.com>
2013-10-30 17:36:29 -0600

    updated file

--- loops.php
+++ loops.php
@@ -8,3 +8,28 @@
     foreach ($hey as $value) {
         echo ($value) . "<br/>";   
     }
+
+
+    $days = array ("sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday");
+
+    for ($i = 0; $i <= 6; $i++) {
+        echo $days[$i] . "<br/>";   
+    } 
+
+    print_r($days);
+
+    $info= array (
+        Name => 'Ismael', 
+        Surname => 'Perez', 
+        BirthMonth =>'November', 
+        BirthDay => 8, 
+        BirthYear => 1983
+    );
+
+    foreach ($info as $value) {
+        echo $value . "<br/>";   
+    }
+
+    
+
+    
