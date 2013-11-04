cc3c21706761931ebe3fe92e309fe4d8350ce6b9
Ismael Perez <ismaelp@vsisoft.com>
2013-11-04 17:07:36 -0600

    split

---
+++ split.php
@@ -0,0 +1,14 @@
+<?php
+
+    $split = "https://github.com/ismismi/btsmentoring/blob/master/lecture1.php";
+
+    $splitArray = explode('/', $split);
+
+    print_r ($splitArray);
+
+ //   $words = count($splitArray);
+
+/*    for($i = 0; $i < $words; $i++){
+        echo $words[$i];
+        echo "<br/>";
+    } */
